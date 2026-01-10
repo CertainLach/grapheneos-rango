4620a8495ffd61cf553234c1e44e7bf08a54ccc3
Lach <iam@lach.pw>
2026-01-10 06:05:09 +0100

    refactor: bring back susfs

--- 01-build_kernel.sh
+++ 01-build_kernel.sh
@@ -51,6 +51,10 @@ pushd kernel
 		ln -sf "$EXTRAS/KernelSU-Next" ./
 		ln -sf "$EXTRAS/susfs4ksu" ./
 
+		cp susfs4ksu/kernel_patches/fs/* fs/
+		cp susfs4ksu/kernel_patches/include/linux/* include/linux/
+		patch -p1 < susfs4ksu/kernel_patches/50_add_susfs_in_gki-android15-6.6.patch
+
 	}
 	popd
 
